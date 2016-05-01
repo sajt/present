bdab95c2b2039780651fe7ce5cf3cca08d299709
Tamás Ámon <tamas.amon@gmail.com>
2016-05-01 19:08:53 +0200

    Added firebase

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'present',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://present-todo.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
